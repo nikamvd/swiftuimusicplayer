6f9ca2bbe48febc40939a71840659fa169338853
Vinayak Nikam <vinayak.nikam@toro.com>
2023-12-27 23:36:06 +0530

    Applied gradient color to song player view

--- swiftuimusicplayer/swiftuimusicplayer/ViewModels/SongPlayerViewModel.swift
+++ swiftuimusicplayer/swiftuimusicplayer/ViewModels/SongPlayerViewModel.swift
@@ -6,11 +6,27 @@
 //
 
 import Foundation
+import AVFoundation
 
 class SongPlayerViewModel: ObservableObject {
     @Published var selectedSong: Song
     
+    var player: AVPlayer?
+    var isPlaying: Bool = false
+    
     init(selectedSong: Song) {
         self.selectedSong = selectedSong
+        let playerItem = AVPlayerItem(url: selectedSong.songUrl)
+        player = AVPlayer(playerItem: playerItem)
+    }
+    
+    func playSong() {
+        if(isPlaying) {
+            player?.pause()
+        }
+        else {
+            player?.play()
+        }
+        isPlaying = !isPlaying
     }
 }
--- swiftuimusicplayer/swiftuimusicplayer/Views/SongPlayerView.swift
+++ swiftuimusicplayer/swiftuimusicplayer/Views/SongPlayerView.swift
@@ -13,6 +13,14 @@ struct SongPlayerView: View {
     
     var body: some View {
         VStack {
+            // Back Button
+            HStack(alignment: .center) {
+                Button(action: {}) {
+                    Image(systemName: "backward.end.fill")
+                        .padding()
+                }
+                Spacer()
+            }
             // Circular image
             PosterView(imageUrl: viewModel.selectedSong.pictureUrl)
                 .aspectRatio(contentMode: .fill)
@@ -27,23 +35,26 @@ struct SongPlayerView: View {
             Text(viewModel.selectedSong.filmName)
                 .font(.title)
                 .fontWeight(.bold)
+                .foregroundColor(.white)
                 .padding(.top, 10)
             
             // Slider and duration labels
             HStack {
                 Text("0:00")
-                    .foregroundColor(.gray)
+                    .foregroundColor(.white)
                 Slider(value: $speed,
                        in: 0...100)
-                    .accentColor(.purple)
+                    .tint(.purple)
+                    .foregroundColor(.white)
                 Text("3:30")
-                    .foregroundColor(.gray)
+                    .foregroundColor(.white)
             }
             .padding(.horizontal)
             
             // Song text
             Text(viewModel.selectedSong.songName)
                 .font(.body)
+                .foregroundColor(.white)
                 .padding()
             
             Spacer()
@@ -58,7 +69,9 @@ struct SongPlayerView: View {
                     Image(systemName: "backward.end.fill")
                         .padding()
                 }
-                Button(action: {}) {
+                Button(action: {
+                    viewModel.playSong()
+                }) {
                     Image(systemName: "playpause")
                         .resizable()
                         .frame(width: 60, height: 60)
@@ -76,16 +89,19 @@ struct SongPlayerView: View {
                         .padding()
                 }
             }
-            .font(.title)
-            .padding()
+            .padding(.horizontal)
         }
         .padding(10)
+        .background(LinearGradient(gradient: Gradient(colors: [
+                Color(red: 255 / 255, green: 75 / 255, blue: 170 / 255),
+                Color(red: 77 / 255, green: 54 / 255, blue: 185 / 255)
+            ]), startPoint: .topLeading, endPoint: .bottomTrailing))
     }
 }
 
 struct SongPlayerView_Previews: PreviewProvider {
     static var previews: some View {
-        var selectedSong = Song(
+        let selectedSong = Song(
             pictureUrl:URL(string: "https://samusicplayer.blob.core.windows.net/cmusicplayer/Phulrani_Poster.jpg")!,
             songUrl:URL(string: "https://samusicplayer.blob.core.windows.net/cmusicplayer/Phulrani_HirveHirve.mp3")!,
             songName:"Hirve Hirve Gaar Gaaliche",
